0854a18042ed48523ee9fc9a1ef15f3cf042982a
Mikael <mikael.carlstein@gmail.com>
2024-06-10 10:30:18 +0200

    TESTSSSS

--- components/single-product.tsx
+++ components/single-product.tsx
@@ -2,78 +2,89 @@ import { Pricing } from '#/components/pricing';
 import type { Product } from '#/types/product';
 import { ProductRating } from '#/components/product-rating';
 import Image from 'next/image';
+import * as console from 'console';
 
 export async function SingleProduct() {
   const product: Product = await fetch(
     `https://app-router-api.vercel.app/api/products?id=1`,
   ).then((res) => res.json());
 
+  const time = await fetch(
+    'http://worldtimeapi.org/api/timezone/Europe/London',
+  ).then((res) => res.json());
+  console.log(time?.datetime);
+
   return (
-    <div className="grid grid-cols-4 gap-6">
-      <div className="col-span-2 md:order-1 md:col-span-1">
-        <div className="space-y-2">
-          <div className="relative aspect-square">
-            <Image
-              src={`/${product.image}`}
-              className="block rounded-lg grayscale"
-              alt={product.name}
-              fill
-              sizes="(min-width: 1184px) 200px, (min-width: 1024px) 20vw, (min-width: 768px) 25vw, 50vw"
-              priority
-            />
+    <>
+      <p className={'text-2xl text-white'}>
+        {new Date(time?.datetime).toLocaleTimeString()}
+      </p>
+      <div className="grid grid-cols-4 gap-6">
+        <div className="col-span-2 md:order-1 md:col-span-1">
+          <div className="space-y-2">
+            <div className="relative aspect-square">
+              <Image
+                src={`/${product.image}`}
+                className="block rounded-lg grayscale"
+                alt={product.name}
+                fill
+                sizes="(min-width: 1184px) 200px, (min-width: 1024px) 20vw, (min-width: 768px) 25vw, 50vw"
+                priority
+              />
+            </div>
+
+            <div className="flex gap-x-2">
+              <div className="relative aspect-square w-1/3">
+                <Image
+                  src={`/${product.image}`}
+                  className="rounded-lg grayscale"
+                  alt={product.name}
+                  fill
+                  sizes="(min-width: 1184px) 75px, (min-width: 768px) 8.33vw, 16.66vw"
+                  priority
+                />
+              </div>
+              <div className="relative aspect-square w-1/3">
+                <Image
+                  src={`/${product.image}`}
+                  className="rounded-lg grayscale"
+                  alt={product.name}
+                  fill
+                  sizes="(min-width: 1184px) 75px, (min-width: 768px) 8.33vw, 16.66vw"
+                  priority
+                />
+              </div>
+              <div className="relative aspect-square w-1/3">
+                <Image
+                  src={`/${product.image}`}
+                  className="rounded-lg grayscale"
+                  alt={product.name}
+                  fill
+                  sizes="(min-width: 1184px) 75px, (min-width: 768px) 8.33vw, 16.66vw"
+                  priority
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-2 md:order-3 md:col-span-1">
+          <Pricing product={product} />
+        </div>
+
+        <div className="col-span-full space-y-4 md:order-2 md:col-span-2">
+          <div className="truncate text-xl font-medium text-white lg:text-2xl">
+            {product.name}
           </div>
 
-          <div className="flex gap-x-2">
-            <div className="relative aspect-square w-1/3">
-              <Image
-                src={`/${product.image}`}
-                className="rounded-lg grayscale"
-                alt={product.name}
-                fill
-                sizes="(min-width: 1184px) 75px, (min-width: 768px) 8.33vw, 16.66vw"
-                priority
-              />
-            </div>
-            <div className="relative aspect-square w-1/3">
-              <Image
-                src={`/${product.image}`}
-                className="rounded-lg grayscale"
-                alt={product.name}
-                fill
-                sizes="(min-width: 1184px) 75px, (min-width: 768px) 8.33vw, 16.66vw"
-                priority
-              />
-            </div>
-            <div className="relative aspect-square w-1/3">
-              <Image
-                src={`/${product.image}`}
-                className="rounded-lg grayscale"
-                alt={product.name}
-                fill
-                sizes="(min-width: 1184px) 75px, (min-width: 768px) 8.33vw, 16.66vw"
-                priority
-              />
-            </div>
+          <ProductRating rating={product.rating} />
+
+          <div className="space-y-4 text-sm text-gray-200">
+            <p>{product.description}</p>
+            <p>{product.description}</p>
           </div>
         </div>
       </div>
-
-      <div className="col-span-2 md:order-3 md:col-span-1">
-        <Pricing product={product} />
-      </div>
-
-      <div className="col-span-full space-y-4 md:order-2 md:col-span-2">
-        <div className="truncate text-xl font-medium text-white lg:text-2xl">
-          {product.name}
-        </div>
-
-        <ProductRating rating={product.rating} />
-
-        <div className="space-y-4 text-sm text-gray-200">
-          <p>{product.description}</p>
-          <p>{product.description}</p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
